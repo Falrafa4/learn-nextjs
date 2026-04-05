4ea76b63761679e84e1c1e5791b2f4d012128976
Falrafa4 <rafaasad9@gmail.com>
2026-04-05 20:36:38 +0700

    add authentication (login and logout)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -85,7 +87,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
             message: "Missing Fields. Failed to Update Invoice."
         }
     }
-    
+
     const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
@@ -107,8 +109,27 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to delete invoice')
+    // throw new Error('Failed to delete invoice')
 
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
